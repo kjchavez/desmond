8c0342f5028e7af2f868d9262ba9b7b7592ff982
Kevin Chavez <kevin.j.chavez@gmail.com>
2017-12-10 10:28:42 -0800

    Better documentation of DesmondNode class and new helper method

--- desmond/thought/node.py
+++ desmond/thought/node.py
@@ -48,14 +48,26 @@ class InputManager(object):
 
 
 class DesmondNode(object):
+    # Headers that are broadcast by this node for others to discover.
+    # Example: "Echo"
     HEADER_OUTPUT_NAME = "dmd-name"
+    # Example: "desmond.types.Text"
     HEADER_OUTPUT_TYPE = "dmd-type"
+    # Example: "tcp://192.68.1.14:5555"
     HEADER_OUTPUT_ADDR = "dmd-addr"
+
+    # Messages send via an inproc pipe from application code to DesmondNode main loop.
     MSG_SHUTDOWN = b"_$STOP"
     MSG_EMIT = b"_$EMIT"
     def __init__(self, name, inputs, OutputType, transport="tcp"):
+        for i in inputs:
+            if not hasattr(i, 'ParseFromString'):
+                raise ValueError("All input types must implement 'ParseFromString' (e.g. protocol"
+                                 " buffer)")
+        if not hasattr(OutputType, "SerializeToString"):
+            raise ValueError("OutputType must implement 'SerializeToString' (e.g. protocol buffer)")
+
         self.name = name
-        # These should be actual types.
         self.inputs = inputs
         self.inputs_by_name = {bytes(i.DESCRIPTOR.full_name, 'latin1'): i for i in inputs}
         self.OutputType = OutputType
@@ -63,6 +75,11 @@ class DesmondNode(object):
         self.pipe = zhelper.zthread_fork(context, self.run, transport=transport)
 
     def run(self, context, pipe, transport="tcp"):
+        """Maintains pyre node and other zmq sockets used to receive inputs and publish output.
+
+        This should be called via zhelper.zthread_fork where the application code owns the other end
+        of the |pipe|.
+        """
         publisher = context.socket(zmq.PUB)
         if transport == "tcp":
             port_selected = publisher.bind_to_random_port('tcp://*', min_port=8001, max_port=9999,
@@ -89,7 +106,7 @@ class DesmondNode(object):
             if pipe in items and items[pipe] == zmq.POLLIN:
                 msg = pipe.recv_multipart()
                 if not msg:
-                    logging.warning("Invalid command")
+                    logging.warning("Received empty message from pipe")
                     break
                 if msg[0] == DesmondNode.MSG_SHUTDOWN:
                     logging.info("Shutting down node")
@@ -98,7 +115,7 @@ class DesmondNode(object):
                     logging.debug("Emitting data.")
                     publisher.send(msg[1])
                 else:
-                    logging.debug("Unknown internal command")
+                    logging.info("Unknown internal command ignored")
 
             elif node.socket() in items:
                 pm = message.PyreMessage(node.recv())
@@ -121,15 +138,29 @@ class DesmondNode(object):
         logging.info("Exiting Receiver loop")
 
     def shutdown(self):
+        """Terminates the discovery/handling loop of the node."""
         self.pipe.send_multipart([DesmondNode.MSG_SHUTDOWN])
 
     def publish(self, data):
+        """Emits instance of self.OutputType to Desmond network.
+
+        Arguments:
+            data: instance of self.OutputType.
+        """
+
         if not isinstance(data, self.OutputType):
             raise ValueError("data must be of type %s" % self.OutputType)
         self.pipe.send_multipart([DesmondNode.MSG_EMIT, data.SerializeToString()])
 
     def recv(self):
-        """Return input of one of the registered input types."""
+        """Return input of one of the registered input types.
+
+        Returns:
+            instance of one of the self.input datatypes
+
+        Raises:
+            zmq.error.Again if no data is available
+        """
         while True:
             dtype, addr, payload = self.pipe.recv_multipart()
             proto = self.inputs_by_name[dtype]()
@@ -138,3 +169,13 @@ class DesmondNode(object):
             # Otherwise things are broken...
             logging.warning("Invalid payload received for dtype: %s", dtype)
 
+    def recv_or_none(self):
+        """Similar to DesmondNode.recv, except returns None rather than raising zmq.error.Again
+
+        Returns:
+            instance of one of self.input dtypes or None
+        """
+        try:
+            return self.recv()
+        except zmq.error.Again:
+            return None
