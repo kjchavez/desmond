54533cf38022205ac800f23896997081198d8b53
Kevin Chavez <kjchavez@stanford.edu>
2017-12-17 11:45:17 -0800

    Simplify sensor.py

--- desmond/perception/sensor.py
+++ desmond/perception/sensor.py
@@ -1,24 +1,6 @@
-import time
-
-from desmond.perception import sensor_data_pb2
 from desmond import thought
 
-def time_usec():
-    return int(time.time()*1e6)
-
-class Sensor(object):
-    def __init__(self, name, transport="tcp"):
-        self.node = thought.DesmondNode(name, [], sensor_data_pb2.SensorDatum,
-                                        transport=transport)
-        self.name = name
-
-    def emit(self, proto):
-        """Publishes data on bound address."""
-        datum = sensor_data_pb2.SensorDatum()
-        datum.time_usec = time_usec()
-        datum.payload.Pack(proto)
-        self.node.publish(datum)
-
-    def shutdown(self):
-        self.node.shutdown()
-
+class Sensor(thought.DesmondNode):
+    """ A Sensor is DesmondNode with no inputs. It only produces data. """
+    def __init__(self, name, OutputType, transport="tcp"):
+        super(Sensor, self).__init__(name, [], OutputType, transport=transport)
